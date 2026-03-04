51d72abe07ac0942b7b33f73ed9b171bd619792e
unknown <pratikshakandel100@gmail.com>
2026-03-04 20:51:12 +0545

    Practice

--- advancedJS/promise.js
+++ advancedJS/promise.js
@@ -14,20 +14,85 @@
 // Log when completed
 // Randomly fail (20% chance using Math.random())
 
-let createOrder = ()=>{
-    let result =  new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            let create = (Math.floor(Math.random()*10) + 1)
-            console.log(create)
-            if(create > 2){
+let createOrder = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+           let create = (Math.floor(Math.random() * 10) + 1)
+            if (create > 2) {
                 return resolve("You have created your order successfully!")
             }
-            else{
+            else {
                 return reject("Sorry! It's too late")
             }
-        },1000)
-        
-    })
+        }, 1000)
 
+    })
 }
-createOrder()
+createOrder().then(
+    (create) => {
+        console.log("success for order")
+    }
+).catch((error) => {
+    console.log(error)
+})
+
+let processPayment = () => {
+    return new Promise((requst,resolve)=>{
+    setTimeout(() => {
+        let create = (Math.floor(Math.random() * 10) + 1)
+        if (create > 5) {
+            return resolve("You have process your payment")
+        }
+        else {
+            return reject("Sorry! It's too late for your payment")
+        }
+    })})
+}
+processPayment().then(
+    (create) => {
+        console.log("success for payment")
+    }
+).catch((error) => {
+    console.log(error)
+})
+
+let generateInvoice = () => {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let create = (Math.floor(Math.random() * 10) + 1)
+        if (create > 7) {
+            return resolve("Generated Invoice")
+        }
+        else {
+            return reject("Sorry! It's too late for generation")
+        }
+    })})
+}
+generateInvoice().then(
+    (create) => {
+        console.log("success for invoice")
+    }
+).catch((error) => {
+    console.log(error)
+})
+
+let sentConfirmation = () => {
+    return new Promise((resolve,reject) => {
+    setTimeout(() => {
+        let create = (Math.floor(Math.random() * 10) + 1)
+        if (create > 7) {
+            return resolve("Generated Invoice")
+        }
+        else {
+            return reject("Sorry! It's too late for generation")
+        }
+    })})
+}
+sentConfirmation().then(
+    (create) => {
+        console.log("success for email confirmation")
+    }
+).catch((error) => {
+    console.log(error)
+})
