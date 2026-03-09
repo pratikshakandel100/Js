784edb330553c697fde901cd57e1fe974d64937c
unknown <pratikshakandel100@gmail.com>
2026-03-09 18:10:23 +0545

    learn the difference of callbackhell promises async/await with small task

--- advancedJS/asyncAwait.js
+++ advancedJS/asyncAwait.js
@@ -1,4 +1,56 @@
-let learn = async function(){
-   console.log("Hello")
+// 🎯 Task: Food Delivery Workflow (Callback Hell)
+// Create these functions:
+// placeOrder()
+// prepareFood()
+// packFood()
+// assignDeliveryBoy()
+// deliverFood()
+// Rules
+
+// Each function must:
+// Use setTimeout (1 second delay)
+// Accept a callback function
+// Log a message when completed
+// Call the callback when done
+
+let placeOrder = ()=>{
+    setTimeout (()=>{
+        console.log("Your food is placed")
+        
+}, 2000)
 }
-learn()
+let prepareFood = ()=>{
+    setTimeout(()=>{
+        console.log("Your food is being prepared.")
+       
+    },3000)
+}
+
+let packFood = (calback)=>{
+    setTimeout(()=>{
+        console.log("Your food is packed")
+     
+    },4000)
+}
+
+let assignDeliveryBoy = ()=>{
+    setTimeout(()=>{
+        console.log("Your food is assign to delivery boy")
+       
+    },5000)
+}
+let delivered = () => {
+    setTimeout(()=>{
+        console.log("Your food is delivered")
+        
+    },6000)
+}
+
+let process = async ()=>{
+       await placeOrder();
+       await prepareFood();
+       await packFood();
+       await assignDeliveryBoy();
+       await delivered();
+}
+process()
