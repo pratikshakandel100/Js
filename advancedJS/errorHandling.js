de9a3aaf9c469d2466eee4f277e6db0836056f79
unknown <pratikshakandel100@gmail.com>
2026-03-10 21:13:57 +0545

    learn

---
+++ advancedJS/errorHandling.js
@@ -0,0 +1,22 @@
+// 🔐 Task: User Login Authentication System
+// You are building a login system like Instagram or Facebook.
+// When a user logs in, the system performs several steps.
+
+// 🧾 Steps in the System
+// 1️⃣ checkUserExists()
+// Check if the user exists in the database.
+// 2️⃣ validatePassword()
+// Verify if the password is correct.
+// 3️⃣ generateToken()
+// Generate a login token.
+// 4️⃣ fetchUserProfile()
+// Fetch the user's profile data.
+// 5️⃣ loginSuccess()
+// Display login success message.
+
+// Each function must:
+// Return a Promise
+// Use setTimeout (1 second delay)
+// Print a message when completed
+// Have 20% chance of failure
+
--- advancedJS/tempCodeRunnerFile.js
+++ advancedJS/tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-Hello
+successfully
